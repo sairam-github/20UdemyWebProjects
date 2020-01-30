d11a41965bfcfa4ba53ed26edc55f554b42f0b3b
Sairam Madem <smadem@ptc.com>
2020-01-30 14:33:07 -0500

    MovieSeatBooking app project is added

---
+++ MovieSeatBooking/script.js
@@ -0,0 +1,34 @@
+const container = document.querySelector ('.container');
+const seats = document.querySelectorAll ('row .seat:not(.occupied)');
+const count = document.getElementById ('count');
+const total = document.getElementById ('total');
+const movieSelect = document.getElementById ('movie');
+let ticketPrice = +movieSelect.value;
+
+function updateSelectedCount () {
+  const selectedSeats = document.querySelectorAll ('.row .seat.selected');
+
+  const selectedSeatCount = selectedSeats.length;
+  count.innerText = selectedSeatCount;
+  total.innerText = selectedSeatCount * ticketPrice;
+}
+
+// Movie select event
+movieSelect.addEventListener ('change', e => {
+  ticketPrice = +e.target.value;
+  updateSelectedCount ();
+});
+
+// Seat selection event
+container.addEventListener ('click', e => {
+  if (
+    e.target.classList.contains ('seat') &&
+    !e.target.classList.contains ('occupied')
+  ) {
+    e.target.classList.toggle ('selected');
+
+    updateSelectedCount ();
+  }
+});
+
+console.log (seats);
